021becf062143ee6865353c582112c4739d85b4d
Dmitry Lazarev <skydim@gmail.com>
2024-02-08 17:52:26 +0100

    initial commit

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from gradio_client.client import Client
+from gradio_client.utils import __version__
+from gradio_client.grclient import GradioClient
+
+__all__ = [
+    "Client",
+    "GradioClient",
+    "__version__",
+]
